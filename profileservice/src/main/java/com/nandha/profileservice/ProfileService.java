7ff23bf7b6e7feefd1f49148db3b417057d181b5
Nandha Kumar Viswanaathan <nandhakumarviswanaathan@gmail.com>
2021-05-26 22:01:16 +0530

    profile service updated

--- profileservice/src/main/java/com/nandha/profileservice/ProfileService.java
+++ profileservice/src/main/java/com/nandha/profileservice/ProfileService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProfileService {
@@ -11,8 +12,23 @@ public class ProfileService {
     @Autowired
     private ProfileRepository profileRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     public Profile create(Profile model){
-        return profileRepository.save(model);
+        if(this.validate(model)){
+            return profileRepository.save(model);
+        }
+        model.setUserId(0);
+        return model;
+    }
+
+    public boolean validate(Profile model){
+        Optional<User> optionalUser=userRepository.findById(model.userId);
+        if(optionalUser.isPresent()){
+            return true;
+        }
+        return false;
     }
 
     public List<Profile> list(){
---
+++ profileservice/src/main/java/com/nandha/profileservice/UserRepository.java
@@ -0,0 +1,10 @@
+package com.nandha.profileservice;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, Integer> {
+}
