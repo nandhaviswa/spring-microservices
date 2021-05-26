dd2d4f4b9ee090ebb24a4f5fe423fa5b74758693
Nandha Kumar Viswanaathan <nandhakumarviswanaathan@gmail.com>
2021-05-26 23:17:36 +0530

    unecessary files deleted

--- common/src/main/java/com/nandha/common/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nandha.common;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends MongoRepository<User, Integer> {
-}
-
