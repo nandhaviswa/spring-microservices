19f19a4920d09f83d4f924e664f028bcf117d448
Nandha Kumar Viswanaathan <nandhakumarviswanaathan@gmail.com>
2021-05-26 22:53:29 +0530

    common files identifed

---
+++ common/src/main/java/com/nandha/common/User.java
@@ -0,0 +1,14 @@
+package com.nandha.common;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Data
+public class User {
+    @Id
+    private int id;
+    private String username;
+    private String email;
+}
