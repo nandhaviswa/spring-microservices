2cd84917e33bb5b0f030534e37eae1edb9b11652
Nandha Kumar Viswanathan <nandhakumarviswanaathan@gmail.com>
2022-08-20 10:39:26 +0530

    unused import removed

--- profileservice/src/main/java/com/nandha/profileservice/UserRepository.java
+++ profileservice/src/main/java/com/nandha/profileservice/UserRepository.java
@@ -1,10 +1,9 @@
 package com.nandha.profileservice;
 
 import com.nandha.common.User;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, Integer> {
